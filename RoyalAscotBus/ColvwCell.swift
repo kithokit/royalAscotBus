bf0d45b549f77643198db32b655a55e9d2847863
Kit Ho <kithokit@mangowish.com>
2015-03-24 12:54:44 +0800

    add table view

---
+++ RoyalAscotBus/ColvwCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  RoyalAscotBus
+//
+//  Created by Kit on 24/3/15.
+//  Copyright (c) 2015 Kit. All rights reserved.
+//
+
+import UIKit
+
+class ColvwCell: UICollectionViewCell {
+    
+    @IBOutlet weak var time: UILabel!
+}
--- RoyalAscotBus/FirstViewController.swift
+++ RoyalAscotBus/FirstViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    
+    var tableData = ["9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56","9:30", "9:45", "9:56"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,25 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return tableData.count
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+        let cell: ColvwCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as ColvwCell
+        cell.time.text = tableData[indexPath.row]
+        return cell
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath){
+        println("Cell \(indexPath.row)")
+    }
+
+
 
 
 }
