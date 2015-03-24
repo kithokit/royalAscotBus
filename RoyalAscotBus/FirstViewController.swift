8186251e01dee07f9c7f43314458665e77701a16
Kit Ho <kithokit@mangowish.com>
2015-03-24 11:00:38 +0800

    Initial Commit

---
+++ RoyalAscotBus/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RoyalAscotBus
+//
+//  Created by Kit on 24/3/15.
+//  Copyright (c) 2015 Kit. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
